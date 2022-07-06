74612e9b18aad9c882cb8af66181753725b2c04c
TopGek99 <arowe890@gmail.com>
2022-07-06 14:00:44 +0930

    almost there quotes

--- src/quotes.c
+++ src/quotes.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:03:37 by nspeedy           #+#    #+#             */
-/*   Updated: 2022/07/06 13:47:43 by alex             ###   ########.fr       */
+/*   Updated: 2022/07/06 13:58:25 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int     arg_count(char **arglist)
 
 t_split *s_quotes(char *arglist, t_split *s)
 {
-    if ((arglist[s->k] == '\'' || arglist[s->k] == '\"') && s->k != 0)
+    if (arglist[s->k] == '\'' || arglist[s->k] == '\"')
     {
         s->k++;
         while (arglist[s->k] != '\0')
@@ -54,14 +54,15 @@ t_split *no_quotes(char *arglist, t_split *s)
     {   
         // printf("i: %d, j: %d\n", s->i, s->j);
         // if (ft_isalpha(arglist[s->j]))
-        // {    
+        // {
+            s_quotes(arglist, s);
             while (arglist[s->j] != ' ' && (arglist[s->j + 1] != '\"' || arglist[s->j + 1] != '\'') && arglist[s->j] != '\0')
             {
                 s->j++;
                 s->l++;
             }
             s->k = s->j + 1;
-            if (arglist[s->j] == ' ' || arglist[s->j] == '\0')
+            if ((arglist[s->j] == ' ' || arglist[s->j] == '\0') && s->l != 0)
             {
                 s->n_args[s->n] = ft_substr(arglist, s->m, s->l);
                 // printf("arglist[j]: %c\n", arglist[s->m]);
@@ -75,7 +76,7 @@ t_split *no_quotes(char *arglist, t_split *s)
             }
             // if (arglist[s->i][s->j] != '\0')
             //         s->j = 0;
-            s_quotes(arglist, s);
+            
         // }
         // printf("arglist[j]: %c\n", arglist[s->j]);
     }
